5de973d995671d423e21d1a61470a4588f056780
YoungDogg <leejonghwo@gmail.com>
2022-03-28 13:10:46 +0900

    feature_order 개발중
    enum별 수정 중

--- src/apis/order/order.module.ts
+++ src/apis/order/order.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Product } from "../product/entities/product.entity";
 import { User } from "../user/entities/user.entity";
 import { Order } from "./entities/order.entity";
 import { OrderResolver } from "./order.resolver";
@@ -9,7 +10,7 @@ import { OrderService } from "./order.service";
 @Module({
     imports:[
         TypeOrmModule.forFeature([
-            Order, User
+            Order, User, Product
         ])
     ],
     providers:[OrderResolver,OrderService]
