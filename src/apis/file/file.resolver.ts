771397efffcd4f31bcf324e75ed609bc0d40b69e
jinyoung <jinyoung@jinyoungs-MacBook-Air.local>
2022-04-04 22:56:47 +0900

    Fix: file upload error

--- src/apis/file/file.resolver.ts
+++ src/apis/file/file.resolver.ts
@@ -1,12 +1,13 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { FileService } from './file.service';
 import { FileUpload, GraphQLUpload } from 'graphql-upload';
+import { File } from './entities/file.entity';
 
 @Resolver()
 export class FileResolver {
   constructor(private fileService: FileService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => [File])
   async uploadFile(
     @Args({ name: 'files', type: () => [GraphQLUpload] }) files: FileUpload[],
   ) {
