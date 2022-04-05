7d414c409b9871afee9328019af8433805b85909
YoungDogg <leejonghwo@gmail.com>
2022-04-05 17:12:46 +0900

    Update file.resolver.ts

--- src/apis/file/file.resolver.ts
+++ src/apis/file/file.resolver.ts
@@ -6,7 +6,7 @@ import { FileUpload, GraphQLUpload } from 'graphql-upload';
 export class FileResolver {
   constructor(private fileService: FileService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => String) // 리턴형태 수정
   async uploadFile(
     @Args({ name: 'files', type: () => [GraphQLUpload] }) files: FileUpload[],
   ) {
